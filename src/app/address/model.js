08162045f1d7ec847e4785a6ffbbc749ce6d1fd8
Wisdom Ekpot <wisdomekpot@gmail.com>
2020-07-30 17:57:05 +0100

    address model

---
+++ src/app/address/model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+let addressSchema;
+
+export default addressSchema = new mongoose.Schema({
+    name: String,
+    companyName: String,
+    streetAddress: String,
+    postalCode: String,
+    city: String,
+    country: String,
+    email: String,
+    phone: String
+});
