9458723892df5dc966c993948686546c0e15df29
Wisdom Ekpot <wisdomekpot@gmail.com>
2020-07-30 15:21:43 +0100

    account module

--- src/app/api/cart/routes.js
+++
@@ -1,9 +0,0 @@
-const cartController = require("./controllers");
-const router = require("express").Router();
-
-router.post("/add-item-to-cart", cartController.addItemToCart);
-router.post("/subtract-item-to-cart", cartController.subtractItem);
-router.get("/get-cart", cartController.getUserCart);
-router.post("/empty-cart", cartController.emptyCart);
-
-module.exports = router
--- src/config/environments/development.js
+++ src/config/environments/development.js
@@ -1,7 +1,7 @@
 module.exports = {
     host: "127.0.0.1",
     mapkey: "AIzaSyDMKSUYaVP2C1pwlklUllVmo8nOoC9zMuU",
-    port: 5000, // change with development port
+    port: 4000, // change with development port
     mongoUrl: "mongodb://localhost:27017/jungle", // replace "projectDbName" with a proper db name
     logLevel: "debug", // can be chenged to error, warning, info, verbose or silly
     secret: "superSuperSecret",
