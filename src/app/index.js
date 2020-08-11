f2a8af9e062c1a39a4108cc76a1fb659024391f1
wisdom Ekpot <wisdomekpot@gmail.com>
2020-08-11 09:36:14 +0100

    register admin routes

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,12 @@
 const userRoutes = require("./account/User/routes");
 const driverRoutes = require("./driver/routes")
 const shipmentRoutes = require("./shipment/routes")
+const adminRoutes = require("./account/Admin/routes")
 
 module.exports = app => {
     app.use("/v1/user", userRoutes);
+    app.use("/v1/admin", shipmentRoutes)
     app.use("/v1/driver", driverRoutes);
-    app.use("/v1/shipment", shipmentRoutes)
+    app.use("/v1/shipment", adminRoutes)
 
 }
