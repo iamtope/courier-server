afcb00a2d17c74edbac5699709e8a92527fd4fbf
ABASS ADAMO <35642751+iamtope@users.noreply.github.com>
2020-08-26 15:54:05 +0100

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,6 @@
-const env = process.env.NODE_ENV || "development";
-const mongoUrl = `mongodb+srv://admin:1Password-@cluster0.5nms6.mongodb.net/woloni?retryWrites=true&w=majority`
+require("dotenv/config");
+const env = process.env.NODE_ENV ? "development" : "production";
 
 const config = require(`./environments/${env.toLowerCase()}`); // eslint-disable-line import/no-dynamic-require
+
 module.exports = config;
